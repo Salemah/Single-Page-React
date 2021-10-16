139c3a17a5e55611e50c6bfc51e992f9f025715c
Salemkd <85119912+Salemah@users.noreply.github.com>
2021-10-16 23:14:24 +0600

    totoal added

--- src/Component/Product/Product.js
+++ src/Component/Product/Product.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
 const Product = (props) => {
     const handleAddCart = props.handleHired;
-    const {name,salaryt,id,images,faculty,position,} = props.product;
-
+    const {name,salaryt,images,faculty,position,} = props.product;
+    const element = <FontAwesomeIcon icon={faShoppingCart} />
     return (
         
             <div className="product">
@@ -11,11 +13,11 @@ const Product = (props) => {
            <h3 className="product-name">{name}</h3>
             <p><small>Fcaulty:{faculty}</small></p>
             <p>Position:{position}</p>
-            <p>Price:${salaryt}</p>
+            <p>Salary:${salaryt}</p>
             
            
            
-            <button onClick={()=>handleAddCart(props.product)} className="btn" >Hire</button>
+            <button onClick={()=>handleAddCart(props.product)} className="btn" >{element} Hire</button>
            </div>
        
     );
