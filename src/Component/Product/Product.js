8948a8c4d500bc372c1f276d9c33e82f7facc06d
Salemkd <85119912+Salemah@users.noreply.github.com>
2021-10-18 00:45:50 +0600

    Search Function Added

--- src/Component/Product/Product.js
+++ src/Component/Product/Product.js
@@ -2,24 +2,32 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
+import Rating from 'react-rating';
 const Product = (props) => {
     const handleAddCart = props.handleHired;
-    const {name,salaryt,images,faculty,position,} = props.product;
+    const { name, salaryt, images, faculty, position, } = props.product;
     const element = <FontAwesomeIcon icon={faShoppingCart} />
     return (
-        
-            <div className="product">
+
+        <div className="product">
             <img src={images} alt="" />
-           <h3 className="product-name">{name}</h3>
+            <h3 className="product-name">{name}</h3>
             <p><small>Fcaulty:{faculty}</small></p>
             <p>Position:{position}</p>
+            <Rating 
+             emptySymbol="fas fa-star"
+             readonly
+             >
+                
+            </Rating>
             <p>Salary:${salaryt}</p>
             
-           
-           
-            <button onClick={()=>handleAddCart(props.product)} className="btn" >{element} Hire</button>
-           </div>
-       
+
+
+
+            <button onClick={() => handleAddCart(props.product)} className="btn" >{element} Hire</button>
+        </div>
+
     );
 };
 
